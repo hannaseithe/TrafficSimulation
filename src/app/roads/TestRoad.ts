7d3da19b955b2b6beac408f571ea7e941b96a77b
Hanna Seithe <hannaseithe@googlemail.com>
2023-09-29 12:16:32 +0200

    WIP OPSegment Ringroad

--- src/app/roads/TestRoad.ts
+++ src/app/roads/TestRoad.ts
@@ -12,17 +12,17 @@ export class TestRoad extends Road {
             end: [400, 200],
             type: 'straight',
             before: [],
-            after: [1]
+            after: []
         }
         this.segments[0] = new StraightSegment(segment_config_1)
 
         const segment_config_2 = {
-            start: [400, 200],
+            start: [400, 400],
             end: [600, 100],
             type: 'bezier',
-            before: [0],
+            before: [3],
             after: [2],
-            points: [[550, 250], [425, 150]]
+            points: [[400, 250], [425, 150]]
         }
 
         this.segments[1] = new BezierSegment(segment_config_2)
@@ -40,18 +40,18 @@ export class TestRoad extends Road {
 
         const segment_config_4 = {
             start: [800, 400],
-            end: [300, 400],
+            end: [400, 400],
             type: 'bezier',
             before: [2],
-            after: [],
-            points: [[1000, 487], [400, 500]]
+            after: [1],
+            points: [[1000, 487], [400, 600]]
         }
 
         this.segments[3] = new BezierSegment(segment_config_4)
 
         const segment_config_5 = {
             type: 'projected',
-            before: [],
+            before: [6],
             after: [5]
         }
 
@@ -68,7 +68,7 @@ export class TestRoad extends Road {
         const segment_config_7 = {
             type: 'projected',
             before: [5],
-            after: []
+            after: [4]
         }
 
         this.segments[6] = this.segments[3].computeOrthogenalProjection(6, segment_config_7 );
@@ -76,8 +76,10 @@ export class TestRoad extends Road {
         this.length = this.segments.reduce((acc, segment) => acc + segment.arclength, 0)
 
         this.segments[1].vehicles.push(new TrafficLight(0, 100, 1,10))
+        this.segments[4].vehicles.push(new TrafficLight(0, 100, 1,10))
         this.segments[3].vehicles.push(new TrafficLight(0, 100, 1, 300))
-        this.speedLimits.push({segment:0, position: 250, speed: 20})
+        this.segments[6].vehicles.push(new TrafficLight(0, 100, 1, 300))
+        //this.speedLimits.push({segment:0, position: 250, speed: 20})
         this.speedLimits.push({segment:1, position: 130, speed: -1})
         this.speedLimits.push({segment:2, position: 400, speed: 20})
         this.speedLimits.push({segment:3, position: 130, speed: -1})
@@ -98,10 +100,11 @@ export class TestRoad extends Road {
     }
 
     public newVehicle(rand,spawnProb) {
-        let new_segment = Math.floor(this.segments.length * rand());
+        let r = rand();
+        let new_segment = r > 0.5 ? 0 : 4;
         if (this.getVehicleNumber() < this.number_veh && rand() < spawnProb && (this.segments[new_segment].vehicles.length == 0 || this.segments[new_segment].vehicles[0].position > 30)) {
             let new_position = 0;
-            let r = rand();
+            r = rand();
             let driver = (r > 0.75) ? DRIV_TYPES.AGG : (r > 0.5) ? DRIV_TYPES.RES : (r > 0.25) ? DRIV_TYPES.REL : DRIV_TYPES.DEF;
             this.segments[new_segment].vehicles.unshift(new Car(this.max_speed * (rand() / 2), new_position, new_segment, driver))
         }
